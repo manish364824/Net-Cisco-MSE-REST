ee33e334d9a6a3283a2a53d359f47ac5511611db
Durand Fabrice <oeufdure@gmail.com>
2015-10-20 19:19:07 -0400

    Added pod

--- lib/Net/Cisco/MSE/REST.pm
+++ lib/Net/Cisco/MSE/REST.pm
@@ -286,33 +286,197 @@ Creates a new L<Net::Cisco::MSE::Rest> instance.
 Creates a new session token for the given user.
 
 
-=head2 maps
+=head2 $rest->maps
+
+The maps object returns detailed map information about campuses, buildings, floors, access points, map dimensions, regions, zones, GPS marker, image information, etc.
+
+=head2 $rest->maps_count
+
+The mapscount returns maps count specifying the number of Campuses, Buildings, and Floors known to MSE.
+
+=head2 $rest->maps_info
+
+The mapsinfo object returns all the floor information associated with the campusName -> buildingName -> floorName. This includes floor dimension, Access Points and their information, GPS Markers etc.
+
+Parameters:
+* floorname—Name of the required floor
+* buildingname—Name of the required building
+* campusname—Name of the required campus
+
+=head2 $rest->maps_image
+
+The mapsimage object returns the floor image data associated with the particular campusName -> buildingName -> floorName.
+
+Parameters:
+
+* floorname—Name of the required floor
+* buildingname—Name of the required building
+* campusname—Name of the required campus
+
+=head2 $rest->maps_image_source
+
+The mapsimagesource object returns the image associated with the specified image name.
+
+Parameters:
+
+* imageName—Name of the required image.
+
+=head2 $rest->real_time_localisation_for_client
+
+The location clients object returns the current location of the wireless client for the specified device ID. The ID can be MAC address, IP address, or Username.
+
+Parameters:
+
+* id: Mac address, IP Address, or Username of the wireless client.
+
+=head2 $rest->real_time_localisation_for_client_count
+
+The locationclientscount object returns count or location of wireless clients on the MSE. Results are filtered based on the specified query param conditions.
+
+=head2 $rest->real_time_localisation_for_tags
+
+Returns a list of Location of Tags for the specified query conditions.
+
+=head2 $rest->real_time_localisation_for_tags_count
+
+The location tags count object returns a count of Tags on MSE based on the specified Query Param conditions.
+
+Parameters:
+
+* []: Defines query conditions for the tag.
+
+=head2 $rest->real_time_localisation_for_rogueaps
+
+Returns a list of Location of Rogue APs for the specified query conditions.
+
+Parameters:
+
+* []: Defines query conditions for the rogue AP.
+
+=head2 $rest->real_time_localisation_for_rogueaps_count
+
+Returns a count of Rogue APs on MSE based on the specified Query Param conditions.
+Returns a list of Location of Rogue APs for the specified query conditions.
+
+Parameters:
+
+* []: Defines query conditions for the rogue AP.
+
+=head2 $rest->real_time_localisation_for_rogueclients
+
+Returns the Location of Rogue Client for the specified id.
+
+Parameters:
+
+* id: MAC address of the rogue client.
+
+=head2 $rest->real_time_localisation_for_rogueclients_count
+
+Returns a count of Rogue Clients on MSE based on the specified Query Param conditions.
+
+Parameters:
+
+* []: Defines query conditions for the rogue client.
+
+=head2 $rest->real_time_localisation_for_interferers
+
+Returns the Location of Interferer for the specified id.
+Returns a count of Rogue Clients on MSE based on the specified Query Param conditions.
+
+Parameters:
+
+* id: MAC address of the interferer.
+
+=head2 $rest->real_time_localisation_for_interferers_count
+
+Returns a count of Interferers on MSE based on the specified Query Param conditions.
+
+Parameters:
+
+* []: Defines query conditions for the interferers.
+
+=head2 $rest->localisation_history_for_client
+
+Returns a list of historical Location records of Wireless Client for the specified id and query conditions
+
+Parameters:
+
+* id: MAC address, IP address, or username of the wireless client.
+* []: Defines query conditions for the wireless client
+
+=head2 $rest->localisation_history_for_client_count
+
+Returns a count of historical Location records of Wireless Clients on MSE based on the specified Query Param conditions.
+
+Parameters:
+
+* []: Defines query conditions for the wireless client.
+
+=head2 $rest->localisation_history_for_tags
+
+Returns a list of the historical Location records of Tag for the specified id and query conditions
+
+Parameters:
+
+* id: MAC address of the tag
+* []: Defines query conditions for the tag.
 
 
-=head2 maps_count
-=head2 maps_info
-=head2 maps_image
-=head2 maps_image_source
-=head2 real_time_localisation_for_client
-=head2 real_time_localisation_for_client_count
-=head2 real_time_localisation_for_tags
-=head2 real_time_localisation_for_tags_count
-=head2 real_time_localisation_for_rogueaps
-=head2 real_time_localisation_for_rogueaps_count
-=head2 real_time_localisation_for_rogueclients
-=head2 real_time_localisation_for_rogueclients_count
-=head2 real_time_localisation_for_interferers
-=head2 real_time_localisation_for_interferers_count
-=head2 localisation_history_for_client
-=head2 localisation_history_for_client_count
-=head2 localisation_history_for_tags
-=head2 localisation_history_for_tags_count
-=head2 localisation_history_for_rogueaps
-=head2 localisation_history_for_rogueaps_count
-=head2 localisation_history_for_rogueclients
-=head2 localisation_history_for_rogueclients_count
-=head2 localisation_history_for_interferers
-=head2 localisation_history_for_interferers_count
+=head2 $rest->localisation_history_for_tags_count
+
+Returns a count of historical location records of Tags on MSE based on the specified Query Param conditions.
+
+Parameters:
+
+* []: Defines query conditions for the interferers.
+
+=head2 $rest->localisation_history_for_rogueaps
+
+Returns a list of historical records of Location of Rogue APs for the specified query conditions.
+
+Parameters:
+
+* []: Defines query conditions for the rogue AP.
+
+=head2 $rest->localisation_history_for_rogueaps_count
+
+Returns a count of historical Location records of Rogue APs based on the specified Query Param conditions.
+
+Parameters:
+
+* []: Defines query conditions for the rogue AP.
+
+=head2 $rest->localisation_history_for_rogueclients
+
+Returns a list of historical Location records of Rogue Clients for the specified query conditions.
+
+Parameters:
+
+* []: Defines query conditions for the rogue client.
+
+=head2 $rest->localisation_history_for_rogueclients_count
+
+Returns a count of Historical Location records of Rogue Clients on MSE based on the specified Query Param conditions.
+
+Parameters:
+
+* []: Defines query conditions for the rogue client.
+
+=head2 $rest->localisation_history_for_interferers
+
+Returns a list of historical Location records of Interferers for the specified query conditions.
+
+Parameters:
+
+* []: Defines query conditions for the interferers.
+
+=head2 $rest->localisation_history_for_interferers_count
+
+Returns a count of historical Location records of Interferers for the specified id and query conditions.
+
+Parameters:
+
+* []: Defines query conditions for the interferers.
 
 
 =head1 LICENSE
